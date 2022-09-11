f54123cd6d855204018543f06f99e80587c636da
Yoav <yoavklein25@gmail.com>
2022-09-11 18:03:49 +0000

    Updating applications to print kafka endpoint

--- applications/baz/3.0/app.py
+++ applications/baz/3.0/app.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
    environment = os.environ['ENV']
+   kafka = os.environ['KAFKA_ENDPOINT']
 
    print(f"Start listening on port {port}")
-   return f'Hello from baz, this is version 3.0\nEnvironment: {environment}\n'
+   return f'Hello from baz, this is version 3.0\nEnvironment: {environment}\nKafka: {kafka}\n'
 
 if __name__ == '__main__':
    port = os.environ['PORT']
