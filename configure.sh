5876da014ad9bda0865021808e05cdbe57f34969
Yoav <yoavklein25@gmail.com>
2022-10-03 06:49:13 +0000

    Update version of foo

--- configure.sh
+++ configure.sh
@@ -7,3 +7,8 @@ source <(argocd completion bash)
 clean_ns() {
     kubectl delete all --all -n argo-demo
 }
+
+call_svc() {
+    svc=$1
+    curl $(kubectl get svc $svc -o jsonpath='{.spec.clusterIP}')
+}
