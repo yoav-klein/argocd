17c734c79fdc8ab13467578c6781012b9f8a849c
Yoav Klein <yoavklein25@gmail.com>
2026-02-24 07:23:35 +0000

    ahh... install

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
